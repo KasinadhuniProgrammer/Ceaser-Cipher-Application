948318406cb159e6a564c7c88d0202f53a7575d2
Satvik Kasinadhuni <59343916+KasinadhuniProgrammer@users.noreply.github.com>
2021-01-30 12:39:13 -0500

    Update Message.java

--- Message.java
+++ Message.java
@@ -1,35 +1,39 @@
+import java.util.Scanner;
 
-public class Message
-{
-    // instance variables - replace the example below with your own
-    private String theMessage;
-    private boolean isEncrypted;
+public class Message {
+  private String userMessage;
+  private String userQuestion;
+  private boolean ifEncrypted;
+  private Scanner theScanner;
 
-    /**
-     * Constructor for objects of class Message
-     */
-    public Message()
-    {
-        // initialise instance variables
-       theMessage = "this is a test";
-       isEncrypted = false;
-    }
 
-    public Message(String theMessage, boolean isEncrypted)
-    {
-        // // initialise instance variables
-       this.theMessage = theMessage;
-       this.isEncrypted = isEncrypted;
+  public Message() {
+    userMessage = "Hello World";
+    ifEncrypted = false;
+
+  }
+
+  public Message(String userMessage, boolean ifEncrypted) {
+    this.userMessage = userMessage;
+    this.ifEncrypted = ifEncrypted;
+  }
+
+  public String getUserMessage() {
+    System.out.println("Enter your message: ");
+    userMessage = theScanner.nextLine();
+    return userMessage;
+  }
+
+  public boolean messageEncrypted() {
+    System.out.println("Enter CRYPT if your is Encrypted, else enter DECRYPT");
+    userQuestion = theScanner.nextLine();
+    if(userQuestion == "CRYPT") {
+      ifEncrypted = true;
+    } else {
+      ifEncrypted = false;
     }
-    
-    public String getMessage()
-    {
-        return theMessage;
-    }
-    
-    
-    public boolean isEncrypted()
-    {
-        return isEncrypted;
-    }
+    return ifEncrypted;
+  }
+
+
 }
