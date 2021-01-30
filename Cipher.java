8b58b5f5dd3e20241447c8ae5768437d57d7e4f4
Satvik Kasinadhuni <59343916+KasinadhuniProgrammer@users.noreply.github.com>
2021-01-30 12:49:26 -0500

    Update Cipher.java

--- Cipher.java
+++ Cipher.java
@@ -1,9 +1,23 @@
 public class Cipher {
   private String plainAlphabet;
   private String cipherAlphabet;
+  
 
   public Cipher(int shift) {
     plainAlphabet = "abcdefghijklmnopqrstuvwxyz";
-    cipherAlphabet = null;
+    cipherAlphabet = createCipherAlphabet(shift);
   }
+
+  private String createCipherAlphabet(int thShift) {
+    return "";
+  }
+
+  public Message encrypt(Message theMessage) {
+    return theMessage;
+  }
+
+  public Message decrpyt(Message theMessage) {
+    return theMessage;
+  }
+
 }
