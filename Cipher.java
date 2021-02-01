661ddbdadc55391e05e8426910bcaeca165ec2f0
Satvik Kasinadhuni <59343916+KasinadhuniProgrammer@users.noreply.github.com>
2021-01-31 23:36:06 -0500

    Update Cipher.java

--- Cipher.java
+++ Cipher.java
@@ -18,12 +18,20 @@ public class Cipher {
   }
 
   public Message encrypt(Message userMessage) {
+    String cipherLetter = "";
     if(userMessage.messageEncrypted() == true) {
       System.out.println("ERROR: MESSAGE IS ALREADY ENCRYPTED");
     } else {
       String plainText = userMessage.getUserMessage();
       String cipherText = "";
-      
+      for(int i = 0; i < plainText.length(); i++) {
+        char currentChar = plainText.charAt(i);
+        int currentIndexOf = plainAlphabet.indexOf(currentChar);
+        //System.out.println(currentIndexOf);
+        System.out.println(+cipherAlphabet.charAt(currentIndexOf));
+
+      }
+
     }
     return userMessage;
   }
