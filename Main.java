33e54ad401a965fa52293393901f759a0e6ed1e1
Satvik Kasinadhuni <59343916+KasinadhuniProgrammer@users.noreply.github.com>
2021-02-01 15:50:32 -0500

    Finished Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    Cipher testCipher = new Cipher(5);
-    Message userMessage = new Message();
-    userMessage.getUserMessage();
-    testCipher.encrypt(userMessage);
+    System.out.println("Welcome to Satvik's Cipher Application!");
+    System.out.println("-------------------------------------------");
+    Scanner mainScanner = new Scanner(System.in);
+    System.out.println("Enter the shift: ");
+    int userCipherShift = mainScanner.nextInt();
+    Cipher testerCipher = new Cipher(userCipherShift);
+    Message mainMessage = new Message();
+    System.out.print("enter E if you want to encrypt this message, else enter D: ");
+    char userChoice = mainScanner.next().charAt(0);
+    if(userChoice == 'E') {
+      testerCipher.encrypt(mainMessage);
+    } else {
+      testerCipher.decrypt(mainMessage);
+    }
+
+    //Cipher testCipher = new Cipher(5);
+    //Message userMessage = new Message();
+    //testCipher.encrypt(userMessage);
   }
 }
