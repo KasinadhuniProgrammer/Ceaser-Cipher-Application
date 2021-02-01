085ee00b67ff1e57790315fb9a95696563782ed8
Satvik Kasinadhuni <59343916+KasinadhuniProgrammer@users.noreply.github.com>
2021-02-01 15:49:56 -0500

    Finished Message.java

--- Message.java
+++ Message.java
@@ -1,4 +1,38 @@
-import java
+import java.util.Scanner;
+
+public class Message { 
+  private String userMessage;
+  private String userQuestion;
+  private boolean ifEncrypted;
+  private Scanner theScanner = new Scanner(System.in);
+
+
+  public Message() {
+    userMessage = "Hello World";
+    ifEncrypted = false;
+
+  }
+
+  public Message(String userMessage, boolean ifEncrypted) {
+    this.userMessage = userMessage;
+    this.ifEncrypted = ifEncrypted;
+  }
+
+  public String getUserMessage() {
+    System.out.println("Enter your message: ");
+    userMessage = theScanner.nextLine();
+    return userMessage;
+  }
+
+  public boolean messageEncrypted() {
+    System.out.println("Enter CRYPT if your message is Encrypted, else enter DECRYPT");
+    userQuestion = theScanner.nextLine();
+    if(userQuestion == "CRYPT") {
+      ifEncrypted = true;
+    } else {
+      ifEncrypted = false;
+    }
+    return ifEncrypted;
   }
 
   public String convertString(String a) {
